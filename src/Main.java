12e99f5b1d3773a1b37779e818fa772ea5d97fd1
Saw3rtt <sawertpra@gmail.com>
2023-05-26 10:49:19 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int bananaVasya = 555555555;
+        byte bananaOleg = 100;
+        short bananaValery = 30000;
+        long bananaIgor = 900000000;
+        float bananaMisha = 3.666f;
+        double bananaOlya = 5.66644444;
+        System.out.println(bananaVasya);
+        System.out.println(bananaOleg);
+        System.out.println(bananaValery);
+        System.out.println(bananaIgor);
+        System.out.println(bananaMisha);
+        System.out.println(bananaOlya);
+
+    }
+}
